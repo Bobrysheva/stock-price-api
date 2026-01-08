5215820bad235f71cad89dce9ff1f40d35551fc1
bobrisheva <bobrisheva@gmail.com>
2026-01-08 22:57:40 +0300

    Add openAPI generator

--- src/main/java/com/bobrysheva/stock_price_api/StockPriceAPI.java
+++ src/main/java/com/bobrysheva/stock_price_api/StockPriceAPI.java
@@ -1,11 +1,7 @@
 package com.bobrysheva.stock_price_api;
 
-import liquibase.Liquibase;
-import liquibase.sdk.supplier.resource.ResourceSupplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.DriverManager;
 
 @SpringBootApplication
 public class StockPriceAPI {
